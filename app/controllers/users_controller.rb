f42ba758bbc216a840a771223c06a2364cca1a98
TaishiNishimura <taishi19880324@gmail.com>
2021-07-18 00:31:23 +0000

    7/18/9:31

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
     @books = @user.books.page(params[:page]).reverse_order
   end
   
+  def new
+    @user = User.new
+  end 
+
+  def edit
+  end
+
+  def create
+  end
+
+  private
+  def user_params
+  params.require(:user).permit(:name, :image, :introduction)
+  end
+
 end
